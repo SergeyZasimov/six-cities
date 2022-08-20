037372d1e58b45ff83a79711173a7131c7eed69c
SergeyZasimov <zasimov.ser@yandex.ru>
2022-08-20 09:37:01 +0300

    Созданы тесты для асинхронных операций в room-process

--- project/src/store/room-process/room-process.test.ts
+++ project/src/store/room-process/room-process.test.ts
@@ -1,13 +1,18 @@
 import { resetFavoritesRoom, resetLoadingStatus, roomProcess, toggleFavoriteRoom } from './room-process';
-import { RoomProcess } from '../../types/state';
+import { RoomProcess, State } from '../../types/state';
 import { createMockOffer } from '../../faker-mocks/create-mock-offer';
-import { LoadingStatus } from '../../const';
+import { ApiRoute, LoadingStatus } from '../../const';
 import { fetchRoomAction } from './async-actions';
 import { Offer } from '../../types/offer';
+import { createApi } from '../../services/api';
+import MockAdapter from 'axios-mock-adapter';
+import thunk from 'redux-thunk';
+import { configureMockStore } from '@jedmao/redux-mock-store';
+import { Action, ThunkDispatch } from '@reduxjs/toolkit';
 
 const offer = createMockOffer();
 
-describe('Reducer: roomProcess', () => {
+describe('RoomProcess: Reducer', () => {
   it('should return initial state', () => {
     expect(roomProcess.reducer(void 0, { type: 'UNKNOWN_TYPE' }))
       .toEqual({ room: {}, status: 'IDLE' });
@@ -20,6 +25,16 @@ describe('Reducer: roomProcess', () => {
     };
     expect(roomProcess.reducer(state, toggleFavoriteRoom(offer)))
       .toEqual({ room: { ...offer, isFavorite: !offer.isFavorite }, status: LoadingStatus.Idle });
+  });
+
+  it('shouldn\'t toggle isFavorite field', () => {
+    const anotherOffer = createMockOffer();
+    const state: RoomProcess = {
+      room: offer,
+      status: LoadingStatus.Idle
+    };
+    expect(roomProcess.reducer(state, toggleFavoriteRoom(anotherOffer)))
+      .toEqual({ room: { ...offer, isFavorite: offer.isFavorite }, status: LoadingStatus.Idle });
   });
 
   it('should reset loading status', () => {
@@ -53,7 +68,7 @@ describe('Reducer: roomProcess', () => {
       });
   });
 
-  it('should toggle loading status to fail', () => {
+  it('should toggle loading status to Fail', () => {
     const state: RoomProcess = {
       room: {} as Offer,
       status: LoadingStatus.Idle
@@ -63,5 +78,40 @@ describe('Reducer: roomProcess', () => {
         room: {},
         status: LoadingStatus.Failed
       });
+  });
+
+  it('should toggle loading status to Loading', () => {
+    const state: RoomProcess = {
+      room: {} as Offer,
+      status: LoadingStatus.Idle
+    };
+    expect(roomProcess.reducer(state, { type: fetchRoomAction.pending.type }))
+      .toEqual({
+        room: {},
+        status: LoadingStatus.Loading
+      });
+  });
+});
+
+describe('RoomProcess: Async Actions', () => {
+  const api = createApi();
+  const mockApi = new MockAdapter(api);
+  const middlewares = [thunk.withExtraArgument(api)];
+  const mockStore = configureMockStore<State, Action, ThunkDispatch<State, typeof api, Action>>(middlewares);
+
+  it('should dispatch LoadRoom when GET request', async () => {
+    mockApi
+      .onGet(`${ApiRoute.Offers}/${offer.id}`)
+      .reply(200, offer);
+    const store = mockStore();
+
+    await store.dispatch(fetchRoomAction(offer.id.toString()));
+
+    const actions = store.getActions().map(( { type } ) => type);
+
+    expect(actions).toEqual([
+      fetchRoomAction.pending.type,
+      fetchRoomAction.fulfilled.type,
+    ]);
   });
 });
